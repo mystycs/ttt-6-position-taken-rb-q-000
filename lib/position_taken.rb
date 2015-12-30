00421e34f66f1173b88ef38653f15f517ff9703c
mystycs <mystycs@gmail.com>
2015-12-30 16:55:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,5 @@
 # code your #position_taken? method here!
+def position_taken?(array, position)
+  return false if (array[position] == " " || array[position] == "" || array[position] == nil)
+  return true if (array[position] == "X" || array[position] == "O")
+end
